6ecf1baccda5eebfb002329e2cd5f67769bec425
jakedahn <jake@markupisart.com>
2014-10-18 15:04:23 -0700

    old work

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,98 +1,21 @@
-/**
- * Main JS file for Casper behaviours
- */
+(function ($) {
+    $(function () {
 
-/* globals jQuery, document */
-(function ($, sr, undefined) {
-    "use strict";
-
-    var $document = $(document),
-
-        // debouncing function from John Hann
-        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-        debounce = function (func, threshold, execAsap) {
-            var timeout;
-
-            return function debounced () {
-                var obj = this, args = arguments;
-                function delayed () {
-                    if (!execAsap) {
-                        func.apply(obj, args);
-                    }
-                    timeout = null;
-                }
-
-                if (timeout) {
-                    clearTimeout(timeout);
-                } else if (execAsap) {
-                    func.apply(obj, args);
-                }
-
-                timeout = setTimeout(delayed, threshold || 100);
-            };
-        };
-
-    $document.ready(function () {
-
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
-
-        function updateImageWidth() {
-            var $this = $(this),
-                contentWidth = $postContent.outerWidth(), // Width of the content
-                imageWidth = this.naturalWidth; // Original image resolution
-
-            if (imageWidth >= contentWidth) {
-                $this.addClass('full-img');
+        $('.post').each(function () {
+            var post_type,
+                tag_blocks = $(this).find('.tags a');
+            if (tag_blocks.length > 0) {
+                tag_blocks.each(function () {
+                    var $tag = $(this).text();
+                    if ($tag === "tweet") { post_type = "fa fa-twitter"; }
+                    if ($tag === "insta") { post_type = "fa fa-instagram"; }
+                    if ($tag === "blog") { post_type = "fa fa-bookmark"; }
+                });
             } else {
-                $this.removeClass('full-img');
+                post_type = "fa fa-bookmark";
             }
-        }
-
-        var $img = $("img").on('load', updateImageWidth);
-        function casperFullImg() {
-            $img.each(updateImageWidth);
-        }
-
-        casperFullImg();
-        $(window).smartresize(casperFullImg);
-
-        $(".scroll-down").arctic_scroll();
-
-    });
-
-    // smartresize
-    jQuery.fn[sr] = function(fn) { return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
-
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
+            $(this).find('h1.post-type').addClass(post_type);
         });
 
-    };
-})(jQuery, 'smartresize');
+    });
+})(jQuery);
