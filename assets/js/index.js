59650c2f0054f248c19c877d4e84268fd6cd20d0
jakedahn <jake@markupisart.com>
2014-10-18 21:14:51 -0700

    got the first pass of the home page done

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,14 +7,34 @@
             if (tag_blocks.length > 0) {
                 tag_blocks.each(function () {
                     var $tag = $(this).text();
-                    if ($tag === "tweet") { post_type = "fa fa-twitter"; }
-                    if ($tag === "insta") { post_type = "fa fa-instagram"; }
-                    if ($tag === "blog") { post_type = "fa fa-bookmark"; }
+                    if ($tag === "tweet") {
+                        post_type = {
+                            icons: "fa fa-twitter",
+                            parent: "twitter"
+                        };
+                    }
+                    if ($tag === "insta") {
+                        post_type = {
+                            icons: "fa fa-instagram",
+                            parent: "instagram"
+                        };
+
+                    }
+                    if ($tag === "blog") {
+                        post_type = {
+                            icons: "fa fa-bookmark",
+                            parent: "bookmark"
+                        };
+                    }
                 });
             } else {
-                post_type = "fa fa-bookmark";
+                post_type = {
+                    icons: "fa fa-bookmark",
+                    parent: "bookmark"
+                };
             }
-            $(this).find('h1.post-type').addClass(post_type);
+            $(this).find('.post-type').addClass(post_type.parent)
+            $(this).find('.post-type').append("<i class='"+post_type.icons+"'></i>");
         });
 
     });
